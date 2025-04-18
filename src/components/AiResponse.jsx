6f9c0e03409ecd29cb49136af6c4944a38414657
VISHNU M K <mail.vishnumk@gmail.com>
2025-04-18 16:30:16 +0530

    ai response ui fix

--- src/components/AiResponse.jsx
+++ src/components/AiResponse.jsx
@@ -1,7 +1,7 @@
 import { marked } from 'marked';
 
 export const AiResponse = ({ text }) => {
-    return <div className='p-2'>
-        <div dangerouslySetInnerHTML={{ __html: marked(text) }} className="backdrop-blur-xl w-full overflow-scroll scroll-none max-h-[600px] whitespace-pre-wrap bg-secondary/20 p-2"></div>
+    return <div className='p-2 backdrop-blur-xl w-full max-h-[600px] bg-secondary/20'>
+        <div dangerouslySetInnerHTML={{ __html: marked(text) }} className="w-full max-h-[584px] overflow-scroll scroll-none whitespace-pre-wrap p-2"></div>
     </div>
 }
